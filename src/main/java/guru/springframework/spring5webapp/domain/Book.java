e634865098cb67643e4c7cfa23fbf1d397eb1f19
muhamedsuhail <muhamed.suhail1@gmail.com>
2022-01-09 17:37:45 +0530

    jpa-commit

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,13 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 
 @Entity
 public class Book {
